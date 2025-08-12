97fd41e95835154fed19e7857c44d35f357b95b6
Shruti Shende <shrutishende11@gmail.com>
2025-08-13 01:16:09 +0530

    Add hero section with 3D model and animation.

---
+++ src/app/constants/index.ts
@@ -0,0 +1,6 @@
+export const words = [
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+];
---
+++ src/app/sections/Hero.tsx
@@ -0,0 +1,84 @@
+"use client";
+import React from "react";
+import { words } from "../constants";
+import Button from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo(
+            ".hero-text h1",
+            { y: 50, opacity: 0 },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 1,
+                ease: "power2.inOut",
+            }
+        );
+    });
+    return (
+        <section id="hero" className="relative overflow-hidden">
+            <div className="absolute top-0 left-0 z-10">
+                <img src="/images/bg.png" alt="background image" />
+            </div>
+            <div className="hero-layout">
+                {/*--------- Left: hero content------- */}
+                <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+                    <div className="flex flex-col gap-7">
+                        <div className="hero-text">
+                            <h1>
+                                Shaping
+                                <span className="slide">
+                                    <span className="wrapper">
+                                        {words.map((word) => (
+                                            <span
+                                                key={word.text}
+                                                className="flex items-center md:gap-3 gap-1 pb-2"
+                                            >
+                                                <img
+                                                    src={word.imgPath}
+                                                    alt={word.text}
+                                                    className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                                                />
+                                                <span>{word.text}</span>
+                                            </span>
+                                        ))}
+                                    </span>
+                                </span>
+                            </h1>
+
+                            <h1>into Real Projects</h1>
+                            <h1>that Deliver Results</h1>
+                        </div>
+                        <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                            Hi, I am Shruti, a Frontend Developer based in Pune
+                            with a passion for code
+                        </p>
+                        <Button
+                            className="md:w-80 md:h-16 w-60 h-12"
+                            id="button"
+                            text="see my work"
+                        />
+                        {/* <Button
+                            className="md:w-80 md:h-16 w-60 h-12"
+                            id="button"
+                            text="Resume"
+                        /> */}
+                    </div>
+                </header>
+                {/*--------- Right: 3D model------- */}
+                <figure>
+                    <div className="hero-3d-layout">
+                        <HeroExperience />
+                    </div>
+                </figure>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
