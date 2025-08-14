78eff110ec40fa88fd407ab961206e3f9551f9f6
Shruti Shende <shrutishende11@gmail.com>
2025-08-14 18:40:54 +0530

    Implemented Grid component to display items using BentoGrid and BentoGridItem.

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -3,4 +3,5 @@ export const words = [
     { text: "Designs", imgPath: "/images/designs.svg" },
     { text: "Code", imgPath: "/images/code.svg" },
     { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Visions", imgPath: "/images/designs.svg" },
 ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Grid from "./components/Grid";
 import Hero from "./sections/Hero";
 
 export default function Home() {
     return (
         <>
-            <Hero />
+            <main>
+                <div>
+                    <Hero />
+                    <Grid />
+                </div>
+            </main>
         </>
     );
 }
